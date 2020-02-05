72bb70a8c396d7b228a1fc51be5f0d3a0807e004
Xiaoheng Pan <xiaoheng.pan@ncr.com>
2020-02-05 12:58:14 -0500

    Starting an empty project

---
+++ Swift-Package-Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift-Package-Manager
+//
+//  Created by Pan, Xiaoheng on 2/5/20.
+//  Copyright © 2020 Pan, Xiaoheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
